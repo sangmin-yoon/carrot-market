a2569f8b23132aacbf078a3a7327cc3418ba0019
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-03-19 22:41:41 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="bg-red-500">
-      <h1 className="text-blue-500"></h1>
+      <h1 className="text-blue-500">test</h1>
     </div>
   );
 };
