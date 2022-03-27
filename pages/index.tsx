592310724c713c8917fe54462a29c4d0146d3d33
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-03-27 11:26:17 +0900

    Dark Mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,24 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 xl:place-content-center py-20 px-10 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      <div className="bg-white flex flex-col justify-between p-6 rounded-2xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="bg-slate-400 dark xl:place-content-center py-20 px-10 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white dark:bg-black flex flex-col justify-between p-6 rounded-2xl shadow-xl">
+        <span className="font-semibold dark:text-white text-3xl">
+          Select Item
+        </span>
         <div className="flex justify-between my-2 ">
-          <span className="text-gray-500">Grey Charir</span>
-          <span className="font-semibold">$19</span>
+          <span className="text-gray-500 dark:text-gray-100">Grey Charir</span>
+          <span className="font-semibold dark:text-white">$19</span>
         </div>
         <div className="flex justify-between my-2 ">
-          <span className="text-gray-500">Grey Charir</span>
-          <span className="font-semibold">$19</span>
+          <span className="text-gray-500 dark:text-gray-100">Grey Charir</span>
+          <span className="font-semibold dark:text-white">$19</span>
         </div>
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-          <span>Total</span>
-          <span className="font-semibold">$9</span>
+          <span className="text-gray-500 dark:text-gray-100">Total</span>
+          <span className="font-semibold dark:text-white">$9</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500  hover:text-black block transition-all ">
+        <button className="mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border dark:hover:bg-white  text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500  hover:text-black block transition-all ">
           Checkout
         </button>
       </div>
