99908de2f29f7257f7d92cc2f79a812f4bef1063
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-03-28 22:03:15 +0900

    tailwind Home Screen

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -10,7 +10,7 @@ export default function Enter() {
   const onPhoneClick = () => setMethod("phone");
   return (
     <div className="mt-16 px-4">
-      <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+      <h3 className="text-3xl font-bold text-center">당근이지</h3>
       <div className="mt-8">
         <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
