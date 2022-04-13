32b59d262b855363ca03bf5a4d863abfb2f0032c
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-04-14 02:00:33 +0900

    Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -6,9 +7,25 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  console.log(req.session);
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
+
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "1293812923232323425088324837sfdsfdsfds23423fd;;kllkjlkhkgkgjhgjhhjfj",
+});
