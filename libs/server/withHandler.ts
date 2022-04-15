1c3b277a06b939cd6b5f80335d17725684b6a1eb
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2022-04-16 01:34:06 +0900

    useUser Hook

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -8,13 +8,13 @@ export interface ResponseType {
 interface ConfigType {
   method: "GET" | "POST" | "DELETE";
   handler: (req: NextApiRequest, res: NextApiResponse) => void;
-  isPrivata?: boolean;
+  isPrivate?: boolean;
 }
 
 export default function withHandler({
   method,
   handler,
-  isPrivata = true,
+  isPrivate = true,
 }: ConfigType) {
   return async function (
     req: NextApiRequest,
@@ -23,8 +23,8 @@ export default function withHandler({
     if (req.method !== method) {
       return res.status(405).end();
     }
-    if (isPrivata && !req.session.user) {
-      res.status(401).json({ ok: false, error: "Plz log in." });
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, error: "Plz log in." });
     }
     try {
       await handler(req, res);
